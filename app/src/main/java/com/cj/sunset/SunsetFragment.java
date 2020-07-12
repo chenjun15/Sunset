54f916e44b84c5e31804bfb57eec5fedc65e3cd9
chenjun <437456560@qq.com>
2020-07-13 06:55:45 +0800

    32.5 挑战练习

--- app/src/main/java/com/cj/sunset/SunsetFragment.java
+++ app/src/main/java/com/cj/sunset/SunsetFragment.java
@@ -19,7 +19,9 @@ public class SunsetFragment extends Fragment {
 
     private View mSceneView;
     private View mSunView;
+    private View mShadowView;
     private View mSkyView;
+    private View mSeaView;
 
     private int mBlueSkyColor;
     private int mSunsetSkyColor;
@@ -36,7 +38,9 @@ public class SunsetFragment extends Fragment {
 
         mSceneView = view;
         mSunView = view.findViewById(R.id.sun);
+        mShadowView = view.findViewById(R.id.shadow);
         mSkyView = view.findViewById(R.id.sky);
+        mSeaView = view.findViewById(R.id.sea);
 
         Resources resources = getResources();
         mBlueSkyColor = resources.getColor(R.color.blue_sky);
@@ -59,10 +63,22 @@ public class SunsetFragment extends Fragment {
     }
 
     private void startAnimation() {
-        float sunYStart = mSunView.getTop(), sunYEnd = mSkyView.getHeight();
+        float sunYStart = mSunView.getTop(), sunYEnd = mSkyView.getHeight() + (float) Math.ceil(mSunView.getHeight() * .1f);
+        float shadowYstart = mShadowView.getTop(), shadowYEnd = -(float) Math.ceil(mShadowView.getHeight() * 1.1f);
 
         ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd).setDuration(3200);
         heightAnimator.setInterpolator(new AccelerateInterpolator());
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(mSunView, "scaleX", 1.f, 1.1f).setDuration(400);
+        scaleXAnimator.setRepeatCount(8);
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(mSunView, "scaleY", 1.f, 1.1f).setDuration(400);
+        scaleYAnimator.setRepeatCount(8);
+
+        ObjectAnimator shadowHeightAnimator = ObjectAnimator.ofFloat(mShadowView, "y", shadowYstart, shadowYEnd).setDuration(3200);
+        shadowHeightAnimator.setInterpolator(new AccelerateInterpolator());
+        ObjectAnimator shadowScaleXAnimator = ObjectAnimator.ofFloat(mShadowView, "scaleX", 1.f, 1.1f).setDuration(400);
+        shadowScaleXAnimator.setRepeatCount(8);
+        ObjectAnimator shadowScaleYAnimator = ObjectAnimator.ofFloat(mShadowView, "scaleY", 1.f, 1.1f).setDuration(400);
+        shadowScaleYAnimator.setRepeatCount(8);
 
         ObjectAnimator sunsetSkyAnimator = ObjectAnimator.ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor).setDuration(3000);
         sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
@@ -72,25 +88,47 @@ public class SunsetFragment extends Fragment {
 
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.play(heightAnimator)
+                .with(scaleXAnimator)
+                .with(scaleYAnimator)
+                .with(shadowHeightAnimator)
+                .with(shadowScaleXAnimator)
+                .with(shadowScaleYAnimator)
                 .with(sunsetSkyAnimator)
                 .before(nightSkyAnimator);
         animatorSet.start();
     }
 
     private void startRevertAnimation() {
-        float sunYStart = mSunView.getTop(), sunYEnd = mSkyView.getHeight();
-
-        ObjectAnimator nightSkyAnimator = ObjectAnimator.ofInt(mSkyView, "backgroundColor", mNightSkyColor, mSunsetSkyColor).setDuration(1500);
-        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+        float sunYStart = mSunView.getTop(), sunYEnd = mSkyView.getHeight() + (float) Math.ceil(mSunView.getHeight() * .1f);
+        float shadowYstart = mShadowView.getTop(), shadowYEnd = -(float) Math.ceil(mShadowView.getHeight() * 1.1f);
 
         ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYEnd, sunYStart).setDuration(3200);
         heightAnimator.setInterpolator(new AccelerateInterpolator());
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(mSunView, "scaleX", 1.f, 1.1f).setDuration(400);
+        scaleXAnimator.setRepeatCount(8);
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(mSunView, "scaleY", 1.f, 1.1f).setDuration(400);
+        scaleYAnimator.setRepeatCount(8);
+
+        ObjectAnimator shadowHeightAnimator = ObjectAnimator.ofFloat(mShadowView, "y", shadowYEnd, shadowYstart).setDuration(3200);
+        shadowHeightAnimator.setInterpolator(new AccelerateInterpolator());
+        ObjectAnimator shadowScaleXAnimator = ObjectAnimator.ofFloat(mShadowView, "scaleX", 1.f, 1.1f).setDuration(400);
+        shadowScaleXAnimator.setRepeatCount(8);
+        ObjectAnimator shadowScaleYAnimator = ObjectAnimator.ofFloat(mShadowView, "scaleY", 1.f, 1.1f).setDuration(400);
+        shadowScaleYAnimator.setRepeatCount(8);
 
         ObjectAnimator sunsetSkyAnimator = ObjectAnimator.ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mBlueSkyColor).setDuration(3000);
         sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
 
+        ObjectAnimator nightSkyAnimator = ObjectAnimator.ofInt(mSkyView, "backgroundColor", mNightSkyColor, mSunsetSkyColor).setDuration(1500);
+        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.play(heightAnimator)
+                .with(scaleXAnimator)
+                .with(scaleYAnimator)
+                .with(shadowHeightAnimator)
+                .with(shadowScaleXAnimator)
+                .with(shadowScaleYAnimator)
                 .with(sunsetSkyAnimator)
                 .after(nightSkyAnimator);
         animatorSet.start();
